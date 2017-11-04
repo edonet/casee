d1ffeda1df5bdb2e6ddaf218d98ee51fc2592842
edoner <edonet@163.com>
2017-11-04 12:26:28 +0800

    更新【目录结构】

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  *****************************************
  * Created by lifx
- * Created on 2017-11-04 10:44:57
+ * Created on 2017-10-12 18:21:02
  *****************************************
  */
 'use strict';
@@ -9,7 +9,106 @@
 
 /**
  *****************************************
- * 抛出接口
+ * 加载依赖
  *****************************************
  */
-export default { version: '0.0.1' };
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import AppProvider, { reducers } from 'ui';
+
+
+/**
+ *************************************
+ * 定义【App】中间件
+ *************************************
+ */
+const
+    win = window,
+    middlewares = [thunk];
+
+
+/**
+ *************************************
+ * 配置开发调试工具
+ *************************************
+ */
+if (process.env.NODE_ENV !== 'production') {
+
+    /* 启用【react】组件渲染性能检测，不支持@16 */
+    // win.Perf = require('react-addons-perf');
+
+    /* 启用【redux】日志 */
+    middlewares.push(require('redux-logger').default);
+
+    /* 启用【reducer】纯函数检测 */
+    middlewares.unshift(require('redux-immutable-state-invariant').default());
+}
+
+
+/**
+ *************************************
+ * 定义数据仓储
+ *************************************
+ */
+let app = require('app'),
+    target = app.target || 'app',
+    storeEnhancers = compose(
+
+        // 装载中间件
+        applyMiddleware(...middlewares),
+
+        // 启用【redux】调试扩展，如果可用
+        win && win.devToolsExtension ? win.devToolsExtension() : f => f,
+    ),
+    store = createStore(
+        combineReducers({ ...app.reducers, ...reducers }), {}, storeEnhancers
+    );
+
+
+/**
+ *****************************************
+ * 启动应用
+ *****************************************
+ */
+
+// 获取根节点
+if (typeof target === 'string') {
+    target = document.getElementById(target);
+}
+
+// 创建根节点
+if (!target || target.nodeType !== 1) {
+    target = document.createElement('div');
+    document.body.appendChild(target);
+}
+
+// 渲染组件
+render((
+    <AppProvider store={store}>{app.default}</AppProvider>
+), target);
+
+
+/**
+ *****************************************
+ * 启用热更新
+ *****************************************
+ */
+if (module.hot) {
+
+    // 接收模块更新
+    module.hot.accept(['ui', 'app'], () => {
+        let app = require('app');
+
+        // 更新状态树
+        store.replaceReducer(
+            combineReducers({ ...app.reducers, ...reducers })
+        );
+
+        // 渲染组件
+        render((
+            <AppProvider store={store}>{app.default}</AppProvider>
+        ), target);
+    });
+}
---
+++ webpack/rules.conf.js
@@ -0,0 +1,106 @@
+'use strict';
+
+
+/*
+ ****************************************
+ * 加载依赖模块
+ ****************************************
+ */
+const
+    postCSSOptions = require('./postcss.conf'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    resolve = require.resolve;
+
+
+/*
+ ****************************************
+ * 定义生成加载器方法
+ ****************************************
+ */
+function loaderCreator(app) {
+    return (name, options) => ({
+        loader: name + '-loader',
+        options: Object.assign({ sourceMap: !app.isProduction }, options)
+    });
+}
+
+
+/*
+ ****************************************
+ * 输出配置项
+ ****************************************
+ */
+module.exports = app => {
+    let loader = loaderCreator(app),
+        cssLoader = [loader('css'), loader('postcss', postCSSOptions)],
+        sassLoader = [...cssLoader, loader('sass'), 'var-loader'];
+
+
+    return [
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: {
+                compact: false,
+                presets: [
+                    [
+                        resolve('babel-preset-env'), { 'modules': false }
+                    ],
+                    resolve('babel-preset-react'),
+                    resolve('babel-preset-stage-3')
+                ],
+                plugins: [resolve('babel-plugin-transform-runtime')],
+                env: {
+                    development: {
+                        plugins: [resolve('react-hot-loader/babel')]
+                    },
+                    production: {
+                        presets: [resolve('babel-preset-react-optimize')]
+                    },
+                    test: {
+                        presets: [resolve('babel-preset-react-app')]
+                    }
+                }
+            }
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: sassLoader
+            })
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: cssLoader
+            })
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 8192,
+                name: 'img/[name].[hash:8].[ext]'
+            }
+        },
+        {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 8192,
+                name: 'fonts/[name].[hash:8].[ext]'
+            }
+        },
+        {
+            test: /\.svgx$/,
+            loader: 'svgx-loader'
+        },
+        {
+            test: /\.tpl$/,
+            loader: 'raw-loader'
+        }
+    ];
+};
