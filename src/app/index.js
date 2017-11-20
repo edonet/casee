9820be5d87a98596bf84ac92c6f0d60f69f3ce29
edoner <edonet@163.com>
2017-11-20 18:21:54 +0800

    更新【app-link】

--- src/app/index.js
+++ src/app/index.js
@@ -14,6 +14,7 @@
  */
 import React from 'react';
 import * as style from 'style';
+import { AppTap } from 'sigo';
 
 console.log(style);
 
@@ -24,5 +25,7 @@ console.log(style);
  *****************************************
  */
 export default function App() {
-    return <div className="app">Hello Orchid! @:@!</div>;
+    return (
+        <AppTap className="app">Hello Orchid! @:@!</AppTap>
+    );
 }
