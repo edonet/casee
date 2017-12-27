d6b8baa01a67988b33cfbc830c2a207000aaf191
edoner <edonet@163.com>
2017-12-27 09:15:52 +0800

    更新【AppRoute】

--- lib/app-router/route/index.jsx
+++ lib/app-router/route/index.jsx
@@ -13,6 +13,8 @@
  *****************************************
  */
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import selector from 'selector';
 import createHistory from './history';
 
 
@@ -25,9 +27,25 @@ class AppRoute extends Component {
 
     // 渲染组件
     render() {
-        return this.props.render();
+        let context = this.context.$$pathname,
+            history = {
+                ...this.props.$router, ...createHistory(context)
+            };
+
+        // 渲染元素
+        return this.props.render(history);
     }
 }
+
+
+/**
+ *****************************************
+ * 获取路由上下文
+ *****************************************
+ */
+AppRoute.contextTypes = {
+    $$pathname: PropTypes.string
+};
 
 
 /**
@@ -35,4 +53,4 @@ class AppRoute extends Component {
  * 抛出组件
  *****************************************
  */
-export default AppRoute;
+export default selector('$router')(AppRoute);
