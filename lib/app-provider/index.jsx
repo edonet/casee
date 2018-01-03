86c6affabb1007d2d6c59ada79ad71d3bf254ff4
edoner <edonet@163.com>
2018-01-03 14:38:20 +0800

    更新【AppProvider】

---
+++ lib/app-provider/index.jsx
@@ -0,0 +1,60 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2017-12-29 18:28:10
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+import { dispatch } from 'selector';
+import AppRouter from '../app-router';
+import AppContent from '../app-content';
+import store from './store';
+import { updateAppDevice } from './actions';
+
+
+/**
+ *****************************************
+ * 产品组件
+ *****************************************
+ */
+class AppProvider extends Component {
+
+    /* 渲染组件 */
+    render() {
+
+        // 渲染元素
+        return (
+            <Provider store={ store }>
+                <AppContainer>
+                    <div className="abs box">
+                        <AppRouter />
+                        <AppContent>{ this.props.children }</AppContent>
+                    </div>
+                </AppContainer>
+            </Provider>
+        );
+    }
+
+    /* 挂载完成 */
+    componentDidMount() {
+        dispatch(updateAppDevice({ ready: true }));
+    }
+}
+
+
+/**
+ *****************************************
+ * 抛出接口
+ *****************************************
+ */
+export default AppProvider;
