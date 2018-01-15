609dfda4d78d9319d5c40f59a190c32e0be455ee
edoner <edonet@163.com>
2018-01-15 09:29:48 +0800

    update [precss-loader]

--- webpack/lib/precss-loader.js
+++ webpack/lib/precss-loader.js
@@ -12,12 +12,33 @@ exports.locals = {
     "__esModule": true,
     "default": function use(){
         var args = [].slice.call(arguments, 0),
-            list = [];
+            list = [],
+            add = function(name) {
+                name && list.push(name);
+            };
 
         // 获取列表
-        args.forEach(function (name) {
-            if (name && typeof name === 'string') {
-                list.push(exports.locals[name] || name);
+        args.forEach(function (arg) {
+            var type = typeof arg;
+
+            // 处理字符串
+            if (type === 'string') {
+                return add(exports.locals[arg] || arg);
+            }
+
+            // 处理列表
+            if (Array.isArray(arg)) {
+                return add(use(...arg));
+            }
+
+            // 处理对象
+            if (type === 'object') {
+                return list = list.concat(Object.keys(name).filter(key => !!name[key]));
+            }
+
+            // 处理函数
+            if (type === 'function') {
+                return add(use(name()));
             }
         });
 
